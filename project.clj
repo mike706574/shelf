a65c10723e5fd11937c255251b05b5f92cb2fd9a
EAS7510 <michaeleaster@northwesternmutual.com>
2017-09-16 20:52:54 -0500

    Execute shell commands without caring how or where.

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject org.clojars.mike706574/shelf "0.1.1-SNAPSHOT"
+  :description "A project."
+  :url "https://github.com/mike706574/shelf"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
+  :dependencies [[com.taoensso/timbre "4.10.0"]
+                 [clj-ssh "0.5.14"]]
+  :profiles {:dev {:source-paths ["dev"]
+                   :target-path "target/dev"
+                   :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
+                                  [org.clojure/tools.namespace "0.2.11"]]}}
+  :repl-options {:init-ns user})
---
+++ src/shelf/core.clj
@@ -0,0 +1,69 @@
+(ns shelf.core
+  (:require [clj-ssh.ssh :as ssh]
+            [clojure.java.shell :as sh]
+            [clojure.string :as str]
+            [taoensso.timbre :as log]))
+
+(defprotocol Shell
+  (execute [this args]))
+
+(defn ^:private log-response
+  [{:keys [exit out err] :as response}]
+  (log/debug (str "Exit Code: " exit))
+  (log/trace (str "Out:\n" out))
+  (log/trace (str "Error:\n" err))
+  response)
+
+(def ^:private output-re #"(?s).*___START_OUT___\n(.*)___END_OUT___.*")
+
+(defn ^:private output [s] (second (re-matches output-re s)))
+(defn ^:private unix-eol [s] (.replace s "\r\n" "\n"))
+
+(defrecord SecureShell [session]
+  Shell
+  (execute [this args]
+    (let [command (str/join " " args)]
+      (log/debug (str "Executing command: " command))
+      (ssh/ssh session {:in "echo START"})
+      (let [wrapped-command (str "echo \"___START___\"; " command "; echo \"___END___\"")
+            {exit :exit :as response} (ssh/ssh session {:in wrapped-command})]
+        (log-response response)
+        (-> response
+            (update :out (comp output unix-eol))
+            (merge (if (zero? exit)
+                     {:status :ok :ok true}
+                     {:status :failure :ok false})))))))
+
+(defrecord LocalShell []
+  Shell
+  (execute [this args]
+    (log/debug (str "Executing command: " (str/join " " args)))
+    (let [{exit :exit :as response} (apply sh/sh args)]
+      (log-response response)
+      (merge response (if (zero? exit)
+                        {:status :ok :ok true}
+                        {:status :failure :ok false})))))
+
+(defn local [] (LocalShell.))
+
+(defn ssh
+  [session]
+  (SecureShell. session))
+
+(defn session [host username password]
+  (ssh/session (ssh/ssh-agent {}) host {:username username
+                                        :password password
+                                        :strict-host-key-checking :no}))
+
+(defn exec [shell args] (execute shell args))
+
+(defmacro with-ssh
+  [expr & body]
+  `(let [~'session ~expr]
+     (try
+       (let [~'shell (remote ~'session)]
+           (when-not (ssh/connected? ~'session)
+             (ssh/connect ~'session))
+         ~@body)
+       (finally
+        (ssh/disconnect ~'session)))))
