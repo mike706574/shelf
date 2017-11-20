83d2aaea393d449c84c7248db776b60a2761d465
EAS7510 <michaeleaster@northwesternmutual.com>
2017-11-20 13:11:33 -0600

    Version 0.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fun.mike/shelf "0.0.6"
+(defproject fun.mike/shelf "0.0.7-SNAPSHOT"
   :description "A project."
   :url "https://github.com/mike706574/shelf"
   :license {:name "Eclipse Public License"
