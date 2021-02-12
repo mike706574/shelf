4a1b99bb22d9cc52cfc51dd0aebaac60854f8c6b
Michael Easter <michaeleaster@northwesternmutual.com>
2021-02-11 21:48:06 -0600

    Update deps.

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/spec.alpha "0.1.143"]
-                 [com.taoensso/timbre "4.10.0"]
+  :dependencies [[org.clojure/spec.alpha "0.2.194"]
+                 [com.taoensso/timbre "5.1.2"]
                  [clj-ssh "0.5.14"]
-                 [commons-codec "1.11"]]
+                 [commons-codec "1.15"]]
   :profiles {:dev {:source-paths ["dev"]
                    :target-path "target/dev"
-                   :dependencies [[org.clojure/clojure "1.9.0"]
-                                  [org.clojure/tools.namespace "0.2.11"]]}}
+                   :dependencies [[org.clojure/clojure "1.10.2"]
+                                  [org.clojure/tools.namespace "1.1.0"]]}}
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :creds :gpg}]]
   :repl-options {:init-ns user})
